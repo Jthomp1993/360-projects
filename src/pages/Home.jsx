f64bc2dde762d7ef7d77c3b2ae596ffeab40ac55
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-07-02 13:34:11 +0100

    Add read more section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,46 @@
 import { useContext, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
+import styled from 'styled-components/macro';
 import HomeHeader from '../components/HomeHeader';
 import Banner from '../components/Banner';
 import Contact from '../components/Contact';
 import Services from '../components/Services';
 import MainContext from '../context/MainContext';
 
+const StyledSectionWrapper = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 50vh;
+    background-color: #F0EBD8;
+`
+
+const StyledSectionContent = styled.div`
+    text-align: center;
+
+    h2 {
+        margin-top: 0;
+        padding-
+    }
+
+
+    @media screen and (min-width: 320px) {       
+            h2 {
+                font-size: calc(32px + 8 * ((100vw - 320px) / 680));
+            }
+    }
+
+    @media screen and (min-width: 1000px) {
+        
+        h2 {
+            font-size: 3rem;
+        }
+    }
+`
 
 const Home = () => {
-    const { setActiveButton } = useContext(MainContext);
+    const { activeButton, setActiveButton } = useContext(MainContext);
     const location = useLocation();
 
     useEffect(() => {
@@ -23,6 +55,12 @@ const Home = () => {
             <Banner />
             <Contact />
             <Services />
+            <StyledSectionWrapper>
+                <StyledSectionContent>
+                    <h2>Find out more about us</h2>
+                    <Link className={`btn__dark ${activeButton === 'about' ? 'active' : ''}`} to='/about' style={{ textDecoration: 'none'}}>Read More</Link>
+                </StyledSectionContent>
+            </StyledSectionWrapper>
         </div>
     )
 }
